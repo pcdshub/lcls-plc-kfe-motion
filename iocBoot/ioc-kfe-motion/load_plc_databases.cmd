ec8cfc1ca51642fe48125cbb84bbe53fd4fb78cb
Zachary Lentz <zlentz@slac.stanford.edu>
2021-06-16 14:55:16 -0700

    BLD: rebuild IOC

--- iocBoot/ioc-kfe-motion/load_plc_databases.cmd
+++ iocBoot/ioc-kfe-motion/load_plc_databases.cmd
@@ -1,3 +1,3 @@
-# Total records: 5629
-callbackSetQueueSize(13258)
+# Total records: 5637
+callbackSetQueueSize(13274)
 dbLoadRecords("kfe_motion.db", "PORT=ASYN_PLC,PREFIX=PLC:KFE:MOTION:,IOCNAME=$(IOC),IOC=$(IOC)")
